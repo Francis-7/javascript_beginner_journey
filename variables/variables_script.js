99425e5fed1d86385d41f4d00e72db7833d6b4f4
Francis Awoh <realmabove02@gmail.com>
2025-02-19 03:48:34 -0800

    corrected the reference id for the subheader

--- variables/variables_script.js
+++ variables/variables_script.js
@@ -4,7 +4,7 @@ const html = document.querySelector('html')
 //end here to make more references via other resources on how to go about running a script ouside html 
 
 const button = document.querySelector('#button')
-const subHeader = document.querySelector('#subHeader')
+const subHeader = document.querySelector('#subHeading')
 
 //this next line of code will do something with the button when is clicked by incrementing the count it was clicked
 button.addEventListener('click', () => {
