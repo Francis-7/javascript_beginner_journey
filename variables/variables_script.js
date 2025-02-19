d70194911b52ee69c99f51b3b4ae3af35be9fe6d
Francis Awoh <realmabove02@gmail.com>
2025-02-19 03:51:26 -0800

    doing hard things in debugging

--- variables/variables_script.js
+++ variables/variables_script.js
@@ -3,8 +3,8 @@ document.appendChild(document.createElement('html'))
 const html = document.querySelector('html')
 //end here to make more references via other resources on how to go about running a script ouside html 
 
-const button = document.querySelector('#button')
-const subHeader = document.querySelector('#subHeading')
+const button = document.querySelector('#button');
+const subHeader = document.querySelector('#subHeading');
 
 //this next line of code will do something with the button when is clicked by incrementing the count it was clicked
 button.addEventListener('click', () => {
