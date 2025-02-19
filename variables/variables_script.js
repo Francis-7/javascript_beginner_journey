102ba164ef9994dff0cba39d8f40a094daf089b3
Francis Awoh <realmabove02@gmail.com>
2025-02-19 03:35:38 -0800

    paused the script code and resort to writing the html: would come back to it after more studies on the subject

--- variables/variables_script.js
+++ variables/variables_script.js
@@ -1,3 +1,4 @@
 const document = new Document()
 document.appendChild(document.createElement('html'))
 const html = document.querySelector('html')
+//end here to make more references via other resources on how to go about running a script ouside html 
