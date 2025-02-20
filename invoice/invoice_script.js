50cf2572e1132ee91add576d9006d8f61af91dae
Francis Awoh <realmabove02@gmail.com>
2025-02-20 09:27:27 -0800

    the code here will print a list of items and their prices in a list format with their total at the botom

--- invoice/invoice_script.js
+++ invoice/invoice_script.js
@@ -1,3 +1,16 @@
 const myLists = document.querySelector('#myLists');
 const listItem = document.createElement('li');
 const totalCost = document.createElement('p');
+
+const itemNeeded = 'shoe:15000, Boxer:12000, gas:98000, wardrobe:80000, bed:220000, fridge:300000, funiture:200000, houserent:450000, villagehose:2000000';
+const items = itemNeeded.split(', ');
+let total = 0;
+for (const item of items) {
+    const [name, price] = item.split(':');
+    const cprice = Number(price);
+    total+=cprice;
+    listItem.textContent = `${name} -- ${price}`;
+    myLists.appendChild(listItem);
+}
+totalCost.textContent = `${total}`;
+document.body.appendChild(totalCost);
