551e25d54a32f889a3d32be25a2ba5463335a359
Francis Awoh <realmabove02@gmail.com>
2025-02-20 09:38:45 -0800

    add some sorting to the code

--- invoice/invoice_script.js
+++ invoice/invoice_script.js
@@ -3,7 +3,7 @@ const myLists = document.querySelector('#myLists');
 const totalCost = document.createElement('p');
 
 const itemNeeded = 'shoe:15000, Boxer:12000, gas:98000, wardrobe:80000, bed:220000, fridge:300000, funiture:200000, houserent:450000, villagehose:2000000';
-const items = itemNeeded.split(', ');
+const items = itemNeeded.split(', ').sort();
 let total = 0;
 for (const item of items) {
     const [name, price] = item.split(':');
@@ -13,5 +13,5 @@ for (const item of items) {
     listItem.textContent = `${name.toUpperCase()} -- ${price}`;
     myLists.appendChild(listItem);
 }
-totalCost.textContent = `${total}`;
+totalCost.textContent = `Total -- ${total}`;
 document.body.appendChild(totalCost);
