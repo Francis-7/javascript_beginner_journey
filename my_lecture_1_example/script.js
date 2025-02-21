89be83d5f352a467061eba368d769e90e6197961
Francis Awoh <realmabove02@gmail.com>
2025-02-21 13:03:47 -0800

    code added

--- my_lecture_1_example/script.js
+++ my_lecture_1_example/script.js
@@ -7,5 +7,19 @@ const list = document.createElement('li');
 
 header.textContent = 'My List of things to buy'.toUpperCase()
 para.textContent = 'Click the button below to see the list'
+button.textContent = 'See list'
 document.body.appendChild(header)
 document.body.appendChild(para)
+document.body.appendChild(button)
+
+const myList = 'beverage:30000, meat:5000, oil:3000, transport:4000 vegetable:1000, fruit:2000'
+const myArray = myList.split(', ')
+button.addEventListener('click', () => {
+    myArray.forEach((item) => {
+        const [name, price] = item.split(':');
+        list.textContent = `${name} --- ${price}`
+        unOrdered.appendChild(list)
+    })
+    document.body.insertBefore(unOrdered)
+    button.textContent = ''
+})
