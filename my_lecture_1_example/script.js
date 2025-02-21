09eb342903de5b5f6ac808952e38000310110252
Francis Awoh <realmabove02@gmail.com>
2025-02-21 13:17:09 -0800

    code added to the script

--- my_lecture_1_example/script.js
+++ my_lecture_1_example/script.js
@@ -3,7 +3,6 @@ const header = document.createElement('hi');
 const para = document.createElement('p');
 const button = document.createElement('button');
 const unOrdered = document.createElement('ul');
-const list = document.createElement('li');
 
 header.textContent = 'My List of things to buy'.toUpperCase()
 para.textContent = 'Click the button below to see the list'
@@ -12,14 +11,15 @@ document.body.appendChild(header)
 document.body.appendChild(para)
 document.body.appendChild(button)
 
-const myList = 'beverage:30000, meat:5000, oil:3000, transport:4000 vegetable:1000, fruit:2000'
+const myList = 'beverage:30000, meat:5000, oil:3000, transport:4000, vegetable:1000, fruit:2000'
 const myArray = myList.split(', ')
 button.addEventListener('click', () => {
     myArray.forEach((item) => {
         const [name, price] = item.split(':');
-        list.textContent = `${name} --- ${price}`
+        const list = document.createElement('li');
+        list.textContent = `${name.toUpperCase()} >> ${price}`
         unOrdered.appendChild(list)
     })
-    document.body.insertBefore(unOrdered)
-    button.textContent = ''
+    document.body.insertBefore(unOrdered, button)
+    document.body.removeChild(button)
 })
