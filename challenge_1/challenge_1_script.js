982b4fcb59a77b9be23da08e6ea7c4755fd837e8
Francis Awoh <realmabove02@gmail.com>
2025-02-22 04:29:23 -0800

    fixed some code bugs

--- challenge_1/challenge_1_script.js
+++ challenge_1/challenge_1_script.js
@@ -18,9 +18,9 @@ function result() {
     const yItem = randomValueFromArray(insertY)
     const zItem = randomValueFromArray(insertZ)
 
-    newStory = newStory.replace(':ubawerx:', 'xItem')
-    newStory = newStory.replace(':inserty:', 'yItem')
-    newStory = newStory.replace(':insertz:', 'zItem')
+    newStory = newStory.replaceAll(':insertx:', xItem)
+    newStory = newStory.replace(':inserty:', yItem)
+    newStory = newStory.replace(':insertz:', zItem)
 
   if(customName.value !== '') {
     const name = customName.value;
